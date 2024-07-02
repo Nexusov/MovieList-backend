a1531836b74c6bebb5157051b1241fbf93891585
Nexus <egor2001grashin@gmail.com>
2024-07-02 03:30:13 +0300

    changed jwt token expiration time to 30 days

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 import { jwtSecret } from '../config/index.js';
 
 const generateToken = (userId) => {
-  return jwt.sign({ _id: userId }, jwtSecret, { expiresIn: '1h' });
+  return jwt.sign({ _id: userId }, jwtSecret, { expiresIn: '30d' });
 };
 
 export default generateToken;
